269fa3a1b5c448982a739f427a28b1c310e278d8
Mahmoud AbuSalah <abu2004salah@gmail.com>
2023-10-25 18:35:00 +0300

    First commit

---
+++ src/OOP2_week2/Test1V4.java
@@ -0,0 +1,10 @@
+package OOP2_week2;
+import static OOP2_week2.Util.*;//static import
+public class Test1V4 {
+    public static void main(String[] args) {
+        int n1 = 10;
+        int n2 = 20;
+        int sum = n1 + n2;
+        println(sum);
+    }
+}
