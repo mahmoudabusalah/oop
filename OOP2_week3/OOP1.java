e13aaec4ac7b336ecedfef1f6fcc1388d6d5150d
Mahmoud AbuSalah <abu2004salah@gmail.com>
2023-10-30 23:29:11 +0300

    add week3 code

---
+++ OOP2_week3/OOP1.java
@@ -0,0 +1,23 @@
+package OOOP_week3;
+
+public class OOP1 {
+    public static void main(String arg[]) {
+        Person p1 = new Person();
+        p1.name = "AbuSalah";
+        p1.sayHello();
+
+        Person p2 = new Person();
+        p2.name = "KI-RA";
+        p2.sayHello();
+
+        Person p3 = p1;
+        p3.name = "Mahmoud";
+        p3.sayHello();
+        p1.sayHello();//The output should be "Mahmoud"
+
+        Person p4 = null;
+        p4.name = "Jalal";
+        p4.sayHello();// when i make run it should be ==> NullPointerException
+
+    }
+}
---
+++ OOP2_week3/Person.java
@@ -0,0 +1,10 @@
+package OOOP_week3;
+
+public class Person {
+    String name;
+    int age;
+
+    public void sayHello() {
+        System.out.println("Hello,my name is " + name);
+    }
+}
